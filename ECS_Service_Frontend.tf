7760ad86125e6131160256a1cc1cbbc306263e57
Ibrahim <ibra.653@gmail.com>
2025-11-09 12:15:46 +0500

    fixed alb listener

--- ECS_Service_Frontend.tf
+++ ECS_Service_Frontend.tf
@@ -11,6 +11,7 @@ module "service_frontend" {
   iam_role_arn                  = aws_iam_role.ecs_service_role.arn
   task_exec_iam_role_arn        = aws_iam_role.ecs_task_execution_role.arn
   tasks_iam_role_arn            = aws_iam_role.ecs_task_role.arn
+  
   enable_execute_command        = true
   availability_zone_rebalancing = "DISABLED"
 
