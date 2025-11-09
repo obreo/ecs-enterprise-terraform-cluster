e02680639305d920dfa8ac13bf44537aa683bd75
Ibrahim <ibra.653@gmail.com>
2025-11-09 12:19:32 +0500

    fixed alb listener

--- ECS_Roles.tf
+++ ECS_Roles.tf
@@ -52,7 +52,7 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 resource "aws_iam_role_policy_attachment" "ecs_service_role_policyy" {
-  role       = aws_iam_role.ecs_task_execution.name
+  role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
 }
 # ECS Full Access
