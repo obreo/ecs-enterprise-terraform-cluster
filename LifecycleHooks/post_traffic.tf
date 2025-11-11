5807bc2fd7a5ba5b7c47885b4c6d96a7b08052fd
Ibrahim <ibra.653@gmail.com>
2025-11-11 12:39:26 +0500

    updated

--- LifecycleHooks/post_traffic.tf
+++ LifecycleHooks/post_traffic.tf
@@ -1,9 +1,9 @@
 module "lambda_function" {
   source = "terraform-aws-modules/lambda/aws"
 
-  function_name = "${var.cluster_name}"
+  function_name = "${var.cluster_name}_post_traffic_test"
   description   = "ecs_post_traffic_test lifecycle"
-  handler       = "index.lambda_handler"
+  handler       = "post_traffic_test.lambda_handler"
   runtime       = "python3.12"
 
   source_path = "src/post_traffic_test.py"
